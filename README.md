6fdc6525d1651b0a55deab878d0154962efc25d5
deadlychambers <shanechambers85@gmail.com>
2022-03-06 00:37:34 -0700

    updated file

--- README.md
+++ README.md
@@ -3,11 +3,10 @@ Basics
 You will need to pass the domain name, and the hosted zone as parameters to the run.sh script after ensuring the script can be executed
 ```
 chmod +x run.sh
-./run.sh airmessage.domain.com EXAMPLE123HOSTEDZONE
 ```
 
-Setting either a windows job to run this, or a cron job to run every 45 minutes should ensure your dynamic dns is covered
+Set up a job to run every few minutes or hours should ensure your dynamic dns is covered. Not sure how often the ISP(?) changes the IP
 
 ## Security
 You should be able to create an IAM user, with access keys, that can ONLY change-resource-record-sets in route 53 for the specific zone. Then
-put those creds on your airmessage server.
+ensure the job can use those creds.
--- run.sh
+++ run.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
-if [ -z "$1" ] ; then read "what domain are you using in aws" _domain_name; else _domain_name="$1"; fi
-if [ -z "$2" ] ; then read "what hosted zone are you using in aws" _hosted_zone; else _hosted_zone="$2"; fi
-
+_domain_name="<the domain name>"
+_hosted_zone="<the aws host zone>"
+_profile="<iam user creds>"
+script_dir=$(dirname "$0")
+cd $script_dir
+rm -rf output.log
+touch output.log
+echo "start ${date}" > output.log
 _current_ip=$(dig +short myip.opendns.com @resolver1.opendns.com)
 _recent_ip=$(dig +short "$_domain_name")
-echo $_current_ip
-echo $_recent_ip
+echo $_current_ip >> output.log
+echo $_recent_ip >> output.log
 if [ "$_recent_ip" == "$_current_ip" ] ;
     then
-	 echo "They are the same"
+		echo "They are the same" >> output.log
     else
-	sed "s/_new_ip/${_current_ip}/; s/_aws_domain_name/${_domain_name}/" change-template.json > change-resource-record-sets.json
-	aws route53 change-resource-record-sets --hosted-zone-id $_hosted_zone --change-batch file://change-resource-record-sets.json
-	cat change-resource-record-sets.json
+		sed "s|_new_ip|${_current_ip}|; s|_aws_domain_name|${_domain_name}|" change-template.json > change-resource-record-sets.json
+		aws route53 change-resource-record-sets --hosted-zone-id $_hosted_zone --change-batch "file://change-resource-record-sets.json" --profile $_profile
+		cat change-resource-record-sets.json >> output.log
 fi
-
-
+cd -
